b76263b1a84bf117cff5825213bbada27bb7a43b
Pearl Zeng <zhuzeng@umich.edu>
2019-09-26 12:30:04 -0400

    Add pagination to the 'movies' endpoint
    To allow partial fetching for later

--- mockApi/mockApi.js
+++ mockApi/mockApi.js
@@ -1,6 +1,11 @@
 import { createHttpProxy } from 'resift';
+
 import genreLookup from './genreLookup';
 import movieLookup from './movieLookup';
+
+// Helpers
+import _get from 'lodash/get'; // array helper
+import paginate from './helpers/paginate';
 
 const genreList = Object.values(genreLookup).map(genre => ({
   id: genre.id,
@@ -26,7 +31,22 @@ export const movies = createHttpProxy('/genre/:id/movies', async ({ requestParam
   await mockDelay();
   const { id } = match.params;
   const genre = genreLookup[id];
-  return genre.movies;
+
+  const { query } = requestParams;
+  const pageSize = _get(query, ['pageSize']);
+
+  if (!genre) {
+    throw new Error('Genre not found');
+  }
+
+  if (!pageSize) {
+    return {
+      results: genre.movies,
+    };
+  }
+
+  const currentPageNumber = _get(query, ['page']);
+  return paginate(genre.movies, pageSize, currentPageNumber);
 });
 
 export const movie = createHttpProxy('/movies/:id', async ({ requestParams, match }) => {
