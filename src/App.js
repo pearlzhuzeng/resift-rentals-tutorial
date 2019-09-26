c07374546166996134d552c8311e7babaef982b6
Pearl Zeng <zhuzeng@umich.edu>
2019-09-26 12:41:23 -0400

    Remove process on adding ReSift
    So we can introduce it later

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-// ReSift
-import { ReSiftProvider } from 'resift';
-import dataService from './dataService';
-
 // Components
 import AppBar from 'components/AppBar';
 
 function App() {
   return (
-    <ReSiftProvider dataService={dataService}>
+    <div>
       <AppBar />
-    </ReSiftProvider>
+    </div>
   );
 }
 
