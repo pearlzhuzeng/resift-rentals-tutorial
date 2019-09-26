0276c3422c2981f552c98f8f4383021ec1580889
Pearl Zeng <zhuzeng@umich.edu>
2019-09-26 11:44:13 -0400

    DIY and refactor endpoints
    1. 'genres' returns the genre list with only genres' ids and names,
    without the movies list
    2. Refactor mockDelay so it gets invoked every time

--- mockApi/mockApi.js
+++ mockApi/mockApi.js
@@ -2,38 +2,35 @@ import { createHttpProxy } from 'resift';
 import genreLookup from './genreLookup';
 import movieLookup from './movieLookup';
 
-function timer(milliseconds) {
+const genreList = Object.values(genreLookup).map(genre => ({
+  id: genre.id,
+  name: genre.name,
+}));
+
+function mockDelay() {
   return new Promise((resolve, reject) => {
     try {
-      setTimeout(resolve, milliseconds);
+      setTimeout(resolve, 300);
     } catch (e) {
       reject(e);
     }
   });
 }
 
-const mockDelay = timer(300);
-
 export const genres = createHttpProxy('/genres', async ({ requestParams }) => {
-  await mockDelay;
-  return Object.values(genreLookup);
-});
-
-export const genre = createHttpProxy('/genres/:id', async ({ requestParams, match }) => {
-  await mockDelay;
-  const { id } = match.params;
-  return genreLookup.find(genre => genre.id === id);
+  await mockDelay();
+  return genreList;
 });
 
 export const movies = createHttpProxy('/genre/:id/movies', async ({ requestParams, match }) => {
-  await mockDelay;
+  await mockDelay();
   const { id } = match.params;
-  const genre = genreLookup.find(genre => genre.id === id);
+  const genre = genreLookup[id];
   return genre.movies;
 });
 
-export const movie = createHttpProxy('movies/:id', async ({ requestParams, match }) => {
-  await mockDelay;
+export const movie = createHttpProxy('/movies/:id', async ({ requestParams, match }) => {
+  await mockDelay();
   const { id } = match.params;
-  return movieLookup.find(movie => movie.id === id);
+  return movieLookup[id];
 });
