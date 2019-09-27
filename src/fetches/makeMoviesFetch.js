bbe61fa1c55d832af4bf176ee6b95267f2f638a4
Pearl Zeng <zhuzeng@umich.edu>
2019-09-27 17:29:34 -0400

    Update moviesFetch to allow paginated fetches

--- src/fetches/makeMoviesFetch.js
+++ src/fetches/makeMoviesFetch.js
@@ -1,13 +1,28 @@
 import { defineFetch } from 'resift';
+import _get from 'lodash/get';
 
 const makeMoviesFetch = defineFetch({
   displayName: 'Get Movies',
+  share: {
+    merge: (previous, response) => {
+      if (!previous) return response;
+
+      return {
+        results: [..._get(previous, ['results'], []), ..._get(response, ['results'], [])],
+        paginationMeta: response.paginationMeta,
+      };
+    },
+  },
   make: genreId => ({
     key: [genreId],
-    request: () => ({ http }) =>
+    request: page => ({ http }) =>
       http({
         method: 'GET',
         route: `/genres/${genreId}/movies`,
+        query: {
+          page,
+          pageSize: 10,
+        },
       }),
   }),
 });
