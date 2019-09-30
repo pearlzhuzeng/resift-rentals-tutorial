d3e4cd24fedf525f961e0af696e5edfcb8582b0d
Pearl Zeng <zhuzeng@umich.edu>
2019-09-30 08:00:10 -0400

    Fetch movie when hover over movie thumbnail

--- src/components/MovieThumbnail.js
+++ src/components/MovieThumbnail.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// Fetches
+import { useDispatch } from 'resift';
+import makeMovieFetch from 'fetches/makeMovieFetch';
 // Styles
 import { makeStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
@@ -21,6 +24,12 @@ const useStyles = makeStyles(theme => ({
 function MovieThumbnail({ className, movie }) {
   const classes = useStyles();
   const { id, name, imageUrl } = movie;
+  const dispatch = useDispatch();
+  const movieFetch = makeMovieFetch(id);
+
+  const handleMouseEnter = () => {
+    dispatch(movieFetch());
+  };
 
   return (
     <Link
@@ -30,6 +39,7 @@ function MovieThumbnail({ className, movie }) {
         backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.9)), url(${imageUrl})`,
         backgroundSize: 'cover',
       }}
+      onMouseEnter={handleMouseEnter}
     >
       <h3 className={classes.name}>{name}</h3>
     </Link>
