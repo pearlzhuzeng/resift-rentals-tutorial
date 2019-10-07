86b66e3e1e20304e614613650e7be4e17e208def
Pearl Zeng <zhuzeng@umich.edu>
2019-10-07 18:49:17 -0400

    Remove Loader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { useEffect } from 'react';
 // Components
 import AppBar from 'components/AppBar';
-import Loader from 'components/Loader';
 import Genre from 'components/Genre';
 // ReSift
-import { useDispatch } from 'resift';
+import { useDispatch, useFetch, isNormal, isLoading } from 'resift';
 // Fetches
 import genresFetch from 'fetches/genresFetch';
 // Styles
 import { makeStyles } from '@material-ui/core/styles';
+import { CircularProgress } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {},
   genre: {
     margin: '8px 0',
   },
+  spinner: {
+    color: 'white',
+  },
 }));
 
 function App() {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [genres, status] = useFetch(genresFetch);
 
   useEffect(() => {
     dispatch(genresFetch());
@@ -27,11 +31,9 @@ function App() {
   return (
     <>
       <AppBar />
-      <Loader fetch={genresFetch}>
-        {genres =>
-          genres.map(genre => <Genre key={genre.id} genre={genre} className={classes.genre} />)
-        }
-      </Loader>
+      {isLoading(status) && <CircularProgress className={classes.spinner} />}
+      {isNormal(status) &&
+        genres.map(genre => <Genre key={genre.id} genre={genre} className={classes.genre} />)}
     </>
   );
 }
