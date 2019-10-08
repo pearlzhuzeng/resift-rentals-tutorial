a68b8feb5bf4f2cbe43507d0d0d17581bcffd4ee
Pearl Zeng <zhuzeng@umich.edu>
2019-10-08 13:26:06 -0400

    Add movie fetch factory

---
+++ src/fetches/makeMovieFetch.js
@@ -0,0 +1,14 @@
+import { defineFetch } from 'resift';
+
+const makeMovieFetch = defineFetch({
+  displayName: 'Get Movie',
+  make: movieId => ({
+    key: [movieId],
+    request: () => ({ http }) =>
+      http({
+        method: 'GET',
+        route: `/movies/${movieId}`,
+      }),
+  }),
+});
+export default makeMovieFetch;
