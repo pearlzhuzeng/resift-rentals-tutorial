03dd4a39d98327fb9fd274e1d3128ba0afc69cee
Pearl Zeng <zhuzeng@umich.edu>
2019-10-08 21:39:49 -0400

    Import helpers

---
+++ src/mockApi/index.js
@@ -0,0 +1,5 @@
+import genreLookup from './genreLookup';
+import movieLookup from './movieLookup';
+// Helpers
+import _get from 'lodash/get'; // array helper
+import paginate from './helpers/paginate';
